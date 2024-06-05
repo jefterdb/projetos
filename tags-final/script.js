2cd687d492690cd08ec6bdbd3b1a165c38cb7f13
jefterdb <87785512+jefterdb@users.noreply.github.com>
2024-06-05 18:00:38 -0300

    Update

---
+++ tags-final/script.js
@@ -0,0 +1,98 @@
+const tags = [
+    "comercial_#grad*_#sp*_2024_#inverno*_#teste_vocacional*",
+    "comercial_#grad*_#tri*_2023_#verao*_#matricula_facil*",
+    "comercial_#pos*_#ht*_2023_#m1*_#enem*",
+    "comercial_#grad*_#ht*_2023_#verao*_#enem*",
+    "comercial_#pos*_#ec*_2025_#verao*_#ebook*",
+    "comercial_#grad*_#sp*_2024_#inverno*_#branding*",
+    "comercial_#pos*_#tri*_2023_#m4*_#bolsas*",
+    "comercial_#grad*_#ht*_2022_#m2*_#vitrine_unasp*",
+    "comercial_#grad*_#ec*_2023_#m1*_#inscricao*"
+];
+
+document.querySelectorAll('.filters select, .filters input').forEach(filter => {
+    filter.addEventListener('change', filterTags);
+});
+
+document.getElementById('search-bar').addEventListener('input', () => {
+    clearFilters();
+    filterTags();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayTags(tags.slice(-7)); // Mostrar as últimas 7 tags cadastradas ao carregar a página
+});
+
+function clearFilters() {
+    document.querySelectorAll('.filters select, .filters input').forEach(filter => {
+        filter.value = '';
+    });
+}
+
+function filterTags() {
+    const nivelEnsino = document.getElementById('nivel-ensino').value;
+    const campus = document.getElementById('campus').value;
+    const ano = document.getElementById('ano').value;
+    const modulo = document.getElementById('modulo').value;
+    const tipoCampanha = document.getElementById('tipo-campanha').value;
+    const promocionais = document.getElementById('promocionais').value;
+    const searchQuery = document.getElementById('search-bar').value.toLowerCase();
+
+    const filteredTags = tags.filter(tag => {
+        return (!searchQuery || tag.toLowerCase().includes(searchQuery)) &&
+               (!nivelEnsino || tag.includes(`#${nivelEnsino}`)) &&
+               (!campus || tag.includes(`#${campus}`)) &&
+               (!ano || tag.includes(`_${ano}_`)) &&
+               (!modulo || tag.includes(`#${modulo}`)) &&
+               (!tipoCampanha || tag.includes(`#${tipoCampanha}`)) &&
+               (!promocionais || tag.includes(`#${promocionais}`));
+    });
+
+    displayTags(filteredTags);
+}
+
+function displayTags(tags) {
+    const results = document.getElementById('results');
+    results.innerHTML = '';
+    tags.forEach(tag => {
+        const tagElement = document.createElement('div');
+        tagElement.classList.add('tag');
+        
+        const tagText = document.createElement('span');
+        tagText.textContent = tag;
+        
+        const copyButton = document.createElement('button');
+        copyButton.classList.add('copy-button');
+        copyButton.innerHTML = '<span class="iconify" data-icon="mdi:content-copy" data-inline="false"></span> COPIAR';
+        copyButton.onclick = () => {
+            copyToClipboard(tag);
+            showCopyEffect(copyButton);
+        };
+        
+        const copyEffect = document.createElement('span');
+        copyEffect.classList.add('copied-effect');
+        copyEffect.textContent = 'Copiado!';
+        
+        tagElement.appendChild(tagText);
+        tagElement.appendChild(copyButton);
+        tagElement.appendChild(copyEffect);
+        
+        results.appendChild(tagElement);
+    });
+}
+
+function copyToClipboard(tag) {
+    navigator.clipboard.writeText(tag).catch(err => {
+        console.error('Erro ao copiar a tag: ', err);
+    });
+}
+
+function showCopyEffect(button) {
+    const effect = button.nextElementSibling;
+    button.classList.add('hidden');
+    effect.classList.add('show');
+    setTimeout(() => {
+        effect.classList.remove('show');
+        button.classList.remove('hidden');
+    }, 2000);
+}
